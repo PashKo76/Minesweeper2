c46dfc31ba789cf43dfe7c7b5bd5ad77da3b8d33
Костя <kostapasenko76@gmail.com>
2022-11-27 14:24:47 +0200

    старт рендера и первой логики

--- MineSweeper2/MineSweeper2/Field.cs
+++ MineSweeper2/MineSweeper2/Field.cs
@@ -8,5 +8,36 @@ namespace MineSweeper2
 {
     internal class Field
     {
+        int Width;
+        int Height;
+        Cell[,] cells;
+        public Field(int Width, int Height)
+        {
+            this.Width = Width;
+            this.Height = Height;
+            cells = new Cell[Width, Height];
+            Build();
+        }
+        void Build()
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    cells[x, y] = new Cell(this, x, y);
+                }
+            }
+        }
+        public void Render()
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    cells[x, y].Render();
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
